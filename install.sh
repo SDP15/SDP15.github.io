1823054dd00fe3c3731faae518310f029ae6fcca
tpb1908 <tpb1908dev@gmail.com>
2019-03-20 14:06:06 +0000

    Add install script.

--- install.sh
+++ install.sh
@@ -5,12 +5,10 @@ wget "${JAR}"
 RESOURCES_LOCATION="https://sdp15.github.io/default_resources.zip"
 curl "${RESOURCES_LOCATION}" --output "default_resources.zip"
 unzip "default_resources.zip"
-default_config=$(wget -qO- https://sdp15.github.io/default_config.conf)
+wget -O config.conf https://sdp15.github.io/default_config.conf
 dir=$(pwd)
-default_config=${default_config/graph_default/"${dir}/default/resources/graph.json"}
-default_config=${default_config/products_default/"${dir}/default/resources/products.json"}
-default_config=${default_config/racks_default/"${dir}/default/resources/racks.json"}
-default_config=${default_config/lists_default/"${dir}/default/resources/lists.json"}
+sed -i -e 's@'"graph_default"'@'"${dir}/default_resources/graph.json"'@g' config.conf
+sed -i -e 's@'"products_default"'@'"${dir}/default_resources/products.json"'@g' config.conf
+sed -i -e 's@'"racks_default"'@'"${dir}/default_resources/racks.json"'@g' config.conf
+sed -i -e 's@'"lists_default"'@'"${dir}/default_resources/lists.json"'@g' config.conf
 
-
-echo $default_config > config.conf
